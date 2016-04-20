11410b9cd43ceba28dd35c697f26b640ed03377f
Eric Gourlaouen <e.gourlaouen@weezevent.com>
2016-04-20 17:25:55 +0200

    todo

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from payutc.client import Client as payutc_client
 
 def init_payutc_client():
+	print('Connecting to Nemopay API...')
 	cli = payutc_client()
 	return cli
 
@@ -14,17 +15,22 @@ def get_sales(client, articles):
 
 
 def get_new_prices(client, sales):
+	# TODO ROMAIN : Generates the new sales for the articles, with the last sales given.
 	pass
 
 
 def update_prices(client, new_prices):
+	# TODO ROMAIN : Threads update_price for every article to update
+	pass
+
+
+def update_price(client, article):
 	pass
 
 
 def launch_wall_street():
 	print('Launching Wall Street...')
 	# Idea : every 30 seconds, launching request to get article sales
-	print('Connecting to Nemopay API...')
 	cli = init_payutc_client()
 	# Getting list of articles available for sale on PayUTC
 	articles = get_articles(cli)
