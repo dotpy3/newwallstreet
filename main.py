698314ba6058b73d5970e2ac041f841938dd7c10
Romain Pellerin <contact@romainpellerin.eu>
2016-04-20 19:00:22 +0200

    Add the algo that updates prices

--- main.py
+++ main.py
@@ -13,6 +13,17 @@ def get_articles(client):
 def get_sales(client, articles):
 	pass
 
+
+def calculate_price(product, ref_sales):
+    if product['sales'] == ref_sales:
+        return product['original_price']
+    elif product['sales'] > ref_sales:
+        return product['actual_price'] + 2
+    elif (product['actual_price'] - 2) >= int(product['original_price'] * 0.8):
+        return product['actual_price'] - 2
+    return product['actual_price']
+
+
 """ Get the new prices
 :products Python list of objects
     products = [{'id': 565, 'sales': 50, 'actual_price': 150, 'original_price': 155}, ...]
@@ -23,14 +34,18 @@ def get_sales(client, articles):
 :returns the same list, each product gets a new property 'new_price'
 """
 def get_new_prices(products):
-	# TODO ROMAIN : Generates the new sales for the articles, with the last sales given.
-        # 1. We sort the products according to their sales units like
-        # 2. Get the 3rd decile value
-        # 3. if (current_beer.sales == median) ==> restore the original price
-        #    else if (... < median ) ==> we lower the price. current_price - 2 (until we reach original_price * 0.8)
-        #    else current_price + 2
+    sorted_liste = sorted(products, key=lambda beer: beer['sales'])
 
-	pass
+    median_nb = int(len(liste) * 0.3) - 1
+    median = sorted_liste[median_nb]
+
+    #print "\n\nElected median", median
+
+    for beer in sorted_liste:
+        beer['new_price'] = calculate_price(beer, median['sales'])
+
+    return sorted_liste
+
 
 def update_prices(client, new_prices):
 	# TODO ROMAIN : Threads update_price for every article to update (one update_price per thread)
