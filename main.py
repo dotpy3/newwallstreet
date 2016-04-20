3ea1e07f2db9633066241e4179fcd0529ec2c304
Romain Pellerin <contact@romainpellerin.eu>
2016-04-20 17:52:31 +0200

    Add comments (objectives)

--- main.py
+++ main.py
@@ -13,18 +13,32 @@ def get_articles(client):
 def get_sales(client, articles):
 	pass
 
-
-def get_new_prices(client, sales):
+""" Get the new prices
+:products Python list of objects
+    products = [{'id': 565, 'sales': 50, 'actual_price': 150, 'original_price': 155}, ...]
+    id: product's id
+    sales: number of sales in the last 10minutes
+    actual_price: in cents
+    original_price: in cents
+:returns the same list, each product gets a new property 'new_price'
+"""
+def get_new_prices(products):
 	# TODO ROMAIN : Generates the new sales for the articles, with the last sales given.
+        # 1. We sort the products according to their sales units like
+        # 2. Get the 3rd decile value
+        # 3. if (current_beer.sales == median) ==> restore the original price
+        #    else if (... < median ) ==> we lower the price. current_price - 2 (until we reach original_price * 0.8)
+        #    else current_price + 2
+
 	pass
 
-
 def update_prices(client, new_prices):
-	# TODO ROMAIN : Threads update_price for every article to update
+	# TODO ROMAIN : Threads update_price for every article to update (one update_price per thread)
 	pass
 
 
 def update_price(client, article):
+        # TODO Eric
 	pass
 
 
